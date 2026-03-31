5b33e30fd815d3b53d24d4eb8b387d4582f4a5c4
Lemonglemongg <aaradhyarya.srivastava@gmail.com>
2026-04-01 00:35:49 +0530

    some changes

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import turtle
 import pandas
+import sys
+import os
 
-image = r"blank_states_img.gif"
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+image = resource_path("blank_states_img.gif")
+
 screen = turtle.Screen()
 screen.title("U.S States Game")
 screen.addshape(image)
 turtle.shape(image)
 
-data = pandas.read_csv("50_states.csv")
+data = pandas.read_csv(resource_path("50_states.csv"))
+
 all_states = data.state.to_list()
 guessed_states = []
-
-
 
 while len(guessed_states) < len(all_states):
     answer_state = screen.textinput(
@@ -25,7 +34,6 @@ while len(guessed_states) < len(all_states):
     answer_state = answer_state.title()
 
     if answer_state in all_states and answer_state not in guessed_states:
-
         state_data = data[data.state == answer_state]
 
         t = turtle.Turtle()
@@ -36,12 +44,13 @@ while len(guessed_states) < len(all_states):
 
         guessed_states.append(answer_state)
 
+os.makedirs("evaluation", exist_ok=True)
 
 evaluation = pandas.DataFrame({
     "State": all_states,
     "Status": ["Guessed" if state in guessed_states else "Missed" for state in all_states]
 })
 
-evaluation.to_csv(r"evaluation\analysis.csv")
+evaluation.to_csv("evaluation/analysis.csv")
 
 screen.exitonclick()
